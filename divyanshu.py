ee35c8171d47e716306789ab291342cfad12e8d1
keshav2608kumar <keshav2608kumar@gmail.com>
2025-11-05 20:59:49 +0530

    Create divyanshu.py
    functionalities 💦💦💦

---
+++ divyanshu.py
@@ -0,0 +1,28 @@
+import time
+def who_is_muthhal():
+    
+    attempt=0
+    wait=5
+    max_mutthi_in_one_day=7
+    while attempt<max_mutthi_in_one_day:
+        print("thinking.............")
+        attempt=attempt+1
+        time.sleep(wait)
+        if attempt==1:
+                                                                          
+            print(f"{attempt}.aayush is muthal")
+        elif attempt==2:
+            print(f"{attempt}.arjun is second muthal")
+        elif attempt==3:
+            print(f"{attempt}.abhay is third muthal")
+        elif attempt==4:
+            print(f"{attempt}.divyanshu is coward muthal")
+        elif attempt==5:
+            print(f"{attempt}.... no report suggest keshav is muthal")
+        elif attempt==6:
+            print(f"{attempt}...........shayan have no equipment sorry!")
+        else:
+            print("thankyou")
+who_is_muthhal()
+print("hope you guys agree with my programm")
+          
